0806f2e16a9702b7b85255a438348f62491cadcb
Ian Laidlaw <ilaidlaw@radford.edu>
2024-11-19 19:37:45 -0500

    Adds board squares

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
             DefaultPlugins.set(WindowPlugin {
                 primary_window: Some(Window {
                     title: "Chess".into(),
-                    name: Some("bevy.app".into()),
+                    resolution: (600.0, 600.0).into(),
                     ..default()
                 }),
                 ..default()
@@ -18,10 +18,51 @@ fn main() {
             LogDiagnosticsPlugin::default(),
             FrameTimeDiagnosticsPlugin,
         ))
+        .add_systems(Startup, (setup, setup_board))
         .run();
 }
 
 
-fn setup(commands: &mut Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+fn setup(mut commands: Commands) {
+    // Camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_matrix(Mat4::from_rotation_translation(
+            Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
+            Vec3::new(-7.0, 20.0, 4.0),
+        )),
+        ..default()
+    });
 
+    // Point light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(4.0, 10.0, 4.0)),
+        ..default()
+    });
+}
+
+fn setup_board(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+    let mesh = meshes.add(Plane3d::default().mesh().size(1.0, 1.0));
+    let white_material = materials.add(Color::srgb(1.0, 0.9, 0.9));
+    let black_material = materials.add(Color::srgb(0.0, 0.1, 0.1));
+
+    // build board
+    for i in 0..8 {
+        for j in 0..8 {
+            commands.spawn(PbrBundle {
+                mesh: mesh.clone(),
+                material: if (i + j + 1) % 2 == 0 {
+                    white_material.clone()
+                }
+                else {
+                    black_material.clone()
+                },
+                transform: Transform::from_translation(Vec3::new(i as f32, 0.0, j as f32)),
+                ..default()
+            });
+        }
+    }
 }
